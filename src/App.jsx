e18c8f9e163dae15ba2ef282dc5a460a7a279c87
diasm <diasm2@gmail.com>
2022-05-08 18:23:57 +0700

    [fix] layout 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import { useState, useEffect } from "react"
 import { Flex, Box, Button, Text } from "@chakra-ui/react"
-import { useForm } from "react-hook-form"
-import axios from "axios"
 import LoginNew from "./components/LoginNew"
 import Signupform from "./components/SignupForm"
+import { useState, useEffect } from "react"
+import { useForm } from "react-hook-form"
 
 function App() {
-  const { handleSubmit } = useForm()
+  const [login, setLogin] = useState({
+    toggle: false,
+  })
+  const [signup, setSignup] = useState({ toggle: false })
 
-  const [data, setData] = useState([])
-  const [login, setLogin] = useState([])
+  const loginToggle = () => {
+    setLogin({ toggle: !login.toggle })
+    console.log(login.toggle)
+  }
 
+  const signupToggle = () => {
+    setSignup({ toggle: !signup.toggle })
+    console.log(signup.toggle)
+  }
 
-
-  useEffect(() => {
-    // getData(setData)
-    // getData2(setLogin)
-  }, [])
+  const {
+    register,
+    handleSubmit,
+    formState: { error },
+  } = useForm()
 
   return (
     <div>
@@ -26,22 +34,41 @@ function App() {
             <Text color="white">카카오 로그인</Text>
           </a>
         </Button>
+
+        {/* <form onSubmit={handleSubmit(onSubmit)}> */}
         <Button size="xs" borderRadius="md" backgroundColor="lightyellow">
           <a href="http://localhost:3000/players/googleauth">구글 로그인</a>
         </Button>
-        <Button size="xs" borderRadius="md" backgroundColor="lightsalmon">
-          <a href="/Signupform">이메일 로그인</a>
-        </Button>
-        <Button size="xs" borderRadius="md" backgroundColor="skyblue">
-          <a href="/Signupform">회원가입</a>
-        </Button>
+        {/* </form> */}
+        <form onSubmit={handleSubmit(loginToggle)}>
+          <Button
+            size="xs"
+            borderRadius="md"
+            backgroundColor="lightsalmon"
+            type="submit"
+          >
+            이메일 로그인
+          </Button>
+        </form>
+        <form onSubmit={handleSubmit(signupToggle)}>
+          <Button
+            size="xs"
+            borderRadius="md"
+            backgroundColor="skyblue"
+            type="submit"
+          >
+            회원가입
+          </Button>
+        </form>
       </Flex>
-      <Flex backgroundColor="" marginTop="30px" marginLeft="30px">
-        <LoginNew />
-      </Flex>
-      <Flex backgroundColor="" marginTop="30px" marginLeft="30px">
-        <Signupform />
-      </Flex>
+      <Box>
+        <Flex backgroundColor="" marginTop="30px" marginLeft="30px">
+          {login.toggle ? <LoginNew /> : null}
+        </Flex>
+        <Flex backgroundColor="" marginTop="30px" marginLeft="30px">
+          {signup.toggle ? <Signupform /> : null}
+        </Flex>
+      </Box>
     </div>
   )
 }
